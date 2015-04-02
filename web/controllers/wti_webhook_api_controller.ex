e634d1e373105f079fa6f370a87ce7927a48f44e
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-04-02 17:42:19 +0200

    Remove example payload from controller as it's in the test

--- web/controllers/wti_webhook_api_controller.ex
+++ web/controllers/wti_webhook_api_controller.ex
@@ -5,8 +5,6 @@ defmodule ContentTranslator.WtiWebhookApiController do
 
   plug :action
 
-  # Example payload
-  # {"project_id":1,"string_id":456,"user_id":10,"locale":"en","file_id":null,"api_url":"URL","translation":{"id":123,"locale":"en","text":"English text","status":"status_unproofread","created_at":"2015-03-19T12:55:52Z","updated_at":"2015-03-19T12:59:56Z","version":6,"string":{"id":456,"key":"test.segment","plural":false,"type":"String","dev_comment":"","status":"Current"}}}
   def create(conn, %{ "format" => "json", "payload" => payload }) do
     payload
     |> JSON.parse
