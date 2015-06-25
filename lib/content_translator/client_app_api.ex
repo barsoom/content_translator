fa93941e4941faca37ea1ad0771c9e98f0711447
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-06-25 19:47:45 +0200

    Encode body as form data
    This seems to pass though nginx fine, and arrive as it should.

--- lib/content_translator/client_app_api.ex
+++ lib/content_translator/client_app_api.ex
@@ -3,7 +3,7 @@ defmodule ContentTranslator.ClientAppApi do
     HTTPotion.post(
       Config.client_app_webhook_url,
       [
-        body: "payload=#{JSON.encode(attributes)}"
+        body: "payload=#{URI.encode_www_form(JSON.encode(attributes))}"
       ]
     )
     |> verify_response_code(200)
