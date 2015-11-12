5e8544193d7eaca42652eaf357b9453e509b6b90
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-11-12 14:40:28 +0100

    Invert version number, er.. release 1.0.
    Been used in production for a while without any errors. Not heavy use,
    but real use. At least a few hundred translations has been synced by
    this tool so far.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ContentTranslator.Mixfile do
 
   def project do
     [app: :content_translator,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
