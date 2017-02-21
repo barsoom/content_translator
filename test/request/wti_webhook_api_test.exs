a4d7df9d8171d58549c7c42592083376f5aed896
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2017-02-21 18:07:14 +0100

    Fix WTI webhook
    Seems they pass better headers or something that causes phoenix to
    automatically parse the JSON. Can't reprouce this in tests yet, but
    pushing this so we can start using the sync again.

--- test/request/wti_webhook_api_test.exs
+++ test/request/wti_webhook_api_test.exs
@@ -30,11 +30,11 @@ defmodule WtiWebhookApiTest do
   end
 
   defp payload do
-    '{"project_id":1,"string_id":456,"user_id":10,"locale":"en","file_id":null,"api_url":"URL","translation":{"id":123,"locale":"en","text":"English text","status":"status_unproofread","created_at":"2015-03-19T12:55:52Z","updated_at":"2015-03-19T12:59:56Z","version":6,"string":{"id":456,"key":"help_item_20: question","plural":false,"type":"String","dev_comment":"","status":"Current"}}}'
+    JSON.parse '{"project_id":1,"string_id":456,"user_id":10,"locale":"en","file_id":null,"api_url":"URL","translation":{"id":123,"locale":"en","text":"English text","status":"status_unproofread","created_at":"2015-03-19T12:55:52Z","updated_at":"2015-03-19T12:59:56Z","version":6,"string":{"id":456,"key":"help_item_20: question","plural":false,"type":"String","dev_comment":"","status":"Current"}}}'
   end
 
   defp non_user_payload do
-    '{"project_id":1,"string_id":456,"user_id":null,"locale":"en","file_id":null,"api_url":"URL","translation":{"id":123,"locale":"en","text":"English text","status":"status_unproofread","created_at":"2015-03-19T12:55:52Z","updated_at":"2015-03-19T12:59:56Z","version":6,"string":{"id":456,"key":"help_item_20: question","plural":false,"type":"String","dev_comment":"","status":"Current"}}}'
+    JSON.parse '{"project_id":1,"string_id":456,"user_id":null,"locale":"en","file_id":null,"api_url":"URL","translation":{"id":123,"locale":"en","text":"English text","status":"status_unproofread","created_at":"2015-03-19T12:55:52Z","updated_at":"2015-03-19T12:59:56Z","version":6,"string":{"id":456,"key":"help_item_20: question","plural":false,"type":"String","dev_comment":"","status":"Current"}}}'
   end
 
   defp post(url, params) do
