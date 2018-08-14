b3ac18056b56d3254c464d4a6f16b33bba0398e9
Tomas Skogberg <tomas.skogberg@gmail.com>
2018-08-14 12:28:58 +0200

    Migrate to circleci v2
    CircleCI will end support for v1 2018-08-31.
    (https://circleci.com/blog/sunsetting-1-0/)
    
    More documentation on how to migrate:
    https://circleci.com/docs/2.0/migrating-from-1-2/

--- script/ci/deploy.sh
+++ script/ci/deploy.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-# Add config for heroku commands like "heroku config:set" and git based deploy over https
+# Add heroku cli if missing
+if [ ! -x heroku ]; then
+  echo "---> Install Heroku CLI"
+  wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz >/dev/null
+  sudo mkdir -p /usr/local/lib /usr/local/bin >/dev/null
+  sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib >/dev/null
+  sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku >/dev/null
+fi
+
+# Add config for heroku commands and git based deploy over https
 echo -e "machine api.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN\nmachine code.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN\nmachine git.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN" > ~/.netrc
 chmod 0600 ~/.netrc
 
@@ -21,11 +30,11 @@ function _deploy_to_heroku {
   heroku git:remote --app $app_name
 
   # Workaround for https://github.com/travis-ci/dpl/issues/127#issuecomment-42397378
-  git fetch --unshallow 2> /dev/null; true
+  set +e
+  git fetch --unshallow 2> /dev/null
+  set -e
 
   git push heroku master
-
-  heroku config:set GIT_COMMIT=$revision -a $app_name
 }
 
 function _smoke_test {
--- script/ci/smoke_test.sh
+++ script/ci/smoke_test.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 curl -s "$APP_URL/search?query=foo&from=sv&to=en" | grep webtranslateit.com 1> /dev/null || exit 1
-
