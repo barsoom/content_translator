18cf2db27765ef7f2326b9f94afd6c9024bd4186
Henrik Nyh <henrik@nyh.se>
2019-10-07 13:32:15 +0200

    Doc deprecated joakimk/honeybadger

--- lib/error_reporting_backend.ex
+++ lib/error_reporting_backend.ex
@@ -1,3 +1,3 @@
 defmodule ErrorReportingBackend do
-  # Honeybadger.io still links here. This has moved to https://github.com/joakimk/honeybadger.
+  # Honeybadger.io still links here. This has moved to https://github.com/joakimk/honeybadger, though that library is now deprecated in favour of the official one.
 end
